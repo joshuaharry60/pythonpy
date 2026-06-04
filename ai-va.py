f84eef3d138949af5eb118bb66df3d1be87deed9
Joshua Harry <joshuaharry60@gmail.com>
2026-06-04 16:33:57 +0100

    workinf version

--- ai-va.py
+++ ai-va.py
@@ -6,15 +6,56 @@ import os
 import dotenv
 dotenv.load_dotenv()
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+api_key = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
 
 # Initialize pyttsx3 engine
 engine = pyttsx3.init()
 
+
 def speak(text):
+    print(text)
     engine.say(text)
-    print("AI", text)
     engine.runAndWait()
     
-speak("Hello, I am your AI assistant. How can I help you today?")
-    
+recognizer = sr.Recognizer()
+
+def listen():
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = recognizer.listen(source)
+
+    try:
+        text = recognizer.recognize_google(audio)
+        print("You said:", text)
+        return text
+    except Exception as e:
+        print(e)
+        return None
+
+# speak("hello am ready to assist you")
+def get_ai_response(user_speech):
+    prompt = f"make sure the answer is short and concise, answer the following question: {user_speech}"
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+
+    return response.text
+
+
+while True:
+    print("waiting for speech...")
+    user_speech = listen()
+
+    if user_speech:
+        print("processing...")
+
+        if "stop" in user_speech.lower():
+            speak("Goodbye!")
+            break
+
+        ai_response = get_ai_response(user_speech)
+        speak(ai_response)
